5e5ff3da7ffffa83faa4c305fddc77bbe6873b71
Jaden S <jbroxnari@protonmail.com>
2022-10-17 15:38:17 -0400

    Tank controls in (may need some fine tuning)

--- Player.gd
+++ Player.gd
@@ -1,37 +1,24 @@
 extends KinematicBody2D
  
-const MOVE_SPEED = 300
- 
-onready var raycast = $RayCast2D
- 
-func _ready():
-	yield(get_tree(), "idle_frame")
-	get_tree().call_group("zombies", "set_player", self)
- 
-func _physics_process(delta):
-	var move_vec = Vector2()
+export (int) var speed = 200
+export (float) var rotation_speed = 1.5
+
+var velocity = Vector2()
+var rotation_dir = 0
+
+func get_input():
+	rotation_dir = 0
+	velocity = Vector2()
 	if Input.is_action_pressed("move_up"):
-		move_vec.y -= 1
+		velocity = Vector2(0, -speed).rotated(rotation)
 	if Input.is_action_pressed("move_down"):
-		move_vec.y += 1
+		velocity = Vector2(0, speed).rotated(rotation)
 	if Input.is_action_pressed("move_left"):
-		#move_vec.x -= 1
-		return
+		rotation_dir -= 1
 	if Input.is_action_pressed("move_right"):
-		#move_vec.x += 1
-		return
+		rotation_dir += 1
 		
-		## thing
-	move_vec = move_vec.normalized()
-	move_and_collide(move_vec * MOVE_SPEED * delta)
- 
-	var look_vec = get_global_mouse_position() - global_position
-	global_rotation = atan2(look_vec.y, look_vec.x)
- 
-	if Input.is_action_just_pressed("shoot"):
-		var coll = raycast.get_collider()
-		if raycast.is_colliding() and coll.has_method("kill"):
-			coll.kill()
- 
-func kill():
-	get_tree().reload_current_scene()
+func _physics_process(delta):
+	get_input()
+	rotation += rotation_dir * rotation_speed * delta
+	velocity = move_and_slide(velocity)
