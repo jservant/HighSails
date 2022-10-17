3caa2fdb16694e25e5036eda752866af3a3782a5
Jaden S <jbroxnari@protonmail.com>
2022-10-17 19:43:32 -0400

    Ship movement is better

--- Player.gd
+++ Player.gd
@@ -1,24 +1,38 @@
 extends KinematicBody2D
  
-export (int) var speed = 200
+export (int) var speed = 50
 export (float) var rotation_speed = 1.5
 
-var velocity = Vector2()
+const acc = 1
+var local_velocity = Vector2()
 var rotation_dir = 0
 
 func get_input():
 	rotation_dir = 0
-	velocity = Vector2()
 	if Input.is_action_pressed("move_up"):
-		velocity = Vector2(0, -speed).rotated(rotation)
-	if Input.is_action_pressed("move_down"):
-		velocity = Vector2(0, speed).rotated(rotation)
-	if Input.is_action_pressed("move_left"):
+		#move, no rotation:
+		#local_velocity.y = max(local_velocity.y - acc, -speed)
+		#move w/ lerp, rotation:
+		#local_velocity = Vector2(0, max(local_velocity.y - acc, -speed)).rotated(rotation)
+		#move, rotation:
+		local_velocity = Vector2(0, -speed).rotated(rotation)
+	elif Input.is_action_pressed("move_down"):
+		#cool dodge idea: when pressed this does a short burst backwards. can't move for 1.5 seconds
+		#local_velocity.y = min(local_velocity.y + acc, speed)
+		#local_velocity = Vector2(0, min(local_velocity.y + acc, speed)).rotated(rotation)
+		local_velocity = Vector2(0, speed).rotated(rotation)
+	elif Input.is_action_pressed("move_left"):
 		rotation_dir -= 1
-	if Input.is_action_pressed("move_right"):
+	elif Input.is_action_pressed("move_right"):
 		rotation_dir += 1
-		
+	else:
+		local_velocity.x = lerp(local_velocity.x,0,0.025)
+		local_velocity.y = lerp(local_velocity.y,0,0.025)
+
 func _physics_process(delta):
+	var linear_velocity = Vector2()
 	get_input()
 	rotation += rotation_dir * rotation_speed * delta
-	velocity = move_and_slide(velocity)
+	#linear_velocity = global_transform.basis.orthonormalized().xform(local_velocity)
+	local_velocity = move_and_slide(local_velocity)
+	print(local_velocity.y)
